12cfea8b89455b12756d3b4396dae269e34bd7e9
piepumpkin939-create <piepumpkin939@gmail.com>
2026-02-02 16:30:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 100 - 50;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+  message.textContent = "YAY!!! 💖 I knew you'd say yes 😘";
+});
